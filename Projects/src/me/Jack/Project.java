b12d9616fd61dba8c7b55ee643c1f9ae07121d55
Jack Fountain <jack@rockyroost.com>
2018-11-11 23:29:05 -0500

    Finished Project 10

--- Projects/src/me/Jack/Project.java
+++ Projects/src/me/Jack/Project.java
@@ -1,5 +1,6 @@
 package me.Jack;
 
+import java.sql.Array;
 import java.sql.SQLOutput;
 import java.util.*;
 import me.Jack.Methods;
@@ -14,8 +15,8 @@ public class Project {
         //p.Project06();
         //p.Project07();
         //p.Project08();
-        p.Project09();
-        //p.Project10();
+        //p.Project09();
+        p.Project10();
         //p.Project11();
         //p.Project12();
     }
@@ -393,8 +394,49 @@ public class Project {
 
     }
 
+    //Done
     public void Project10(){
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> input = new ArrayList<>();
+        ArrayList<Integer> outcomes = new ArrayList<>();
+        HashMap<Integer, Integer> results = new HashMap();
+        Random r = new Random();
 
+        int dice1, dice2;
+
+        System.out.println("Enter a number");
+        int userNum = Integer.parseInt(scanner.nextLine());
+
+        for(int i=0; i<userNum; i++){
+            System.out.println("Enter a string");
+            input.add(scanner.nextLine());
+        }
+
+        for(int i=userNum-1; i>-1; i--){
+            System.out.println(input.get(i));
+        }
+
+        for(int i=0; i<1000; i++){
+            dice1 = r.nextInt(6) + 1;
+            dice2 = r.nextInt(6) + 1;
+
+            outcomes.add(dice1 + dice2);
+        }
+
+        Methods.clearChat(5);
+        for(int i=2; i<13; i++){
+            System.out.print(Methods.padR(i + "", 5));
+            results.put(i, 0);
+        }
+
+        for(int i=0; i<outcomes.size(); i++){
+            results.put(outcomes.get(i), results.get(outcomes.get(i))+1);
+        }
+
+        Methods.clearChat(2);
+        for(int i=2; i<13; i++){
+            System.out.print(Methods.padR(results.get(i) + "", 5));
+        }
     }
 
     public void Project11(){
