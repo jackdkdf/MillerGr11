f06e0383692323611ae0904129e47b40d822eef4
Jack Fountain <jack@rockyroost.com>
2019-01-23 17:28:51 -0500

    Updated project 06 (Bouncing off the walls)

--- Projects/src/me/Jack/Project.java
+++ Projects/src/me/Jack/Project.java
@@ -1,11 +1,13 @@
 package me.Jack;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.nio.Buffer;
 import java.text.DecimalFormat;
 import java.util.*;
+import java.util.List;
 
 public class Project {
     public static void main(String args[]) throws Exception{
@@ -22,7 +24,7 @@ public class Project {
         //p.Project07();
         //p.Project08();
         //p.Project09();
-        //p.Project10();
+        p.Project10();
         //p.Project11();
         //p.Project12();
 
@@ -200,8 +202,42 @@ public class Project {
         System.out.println("Your fine is $" + (fine+5));
     }
 
-    //Done (On Mac)
-    public void Project06(){}
+    //Done
+    public void Project06(){
+//    PaintBug.drawGrid();
+//    PaintBug.setWindowSize(600, 600);
+//    PaintBug line = new PaintBug();
+//    line.setSpeed(95);
+//    Random rand = new Random();
+//
+//    while(true){
+//        line.move(1);
+//        float r = rand.nextFloat();
+//        float g = rand.nextFloat();
+//        float b = rand.nextFloat();
+//        if(line.getXPos() >= 500){
+//            System.out.println("Past 500 X");
+//            double rando = Math.random()* 20 + 180;
+//            line.turnLeft(rando);
+//            line.setColor(new Color(r, g, b));
+//        } else if (line.getXPos() <= 0){
+//            System.out.println("Past 0 X");
+//            double rando = Math.random()* 20 + 180;
+//            line.turnLeft(rando);
+//            line.setColor(new Color(r, g, b));
+//        } else if (line.getYPos() >= 500){
+//            System.out.println("Past 500 Y");
+//            double rando = Math.random()* 20 + 180;
+//            line.turnLeft(rando);
+//            line.setColor(new Color(r, g, b));
+//        } else if (line.getYPos() <= 0){
+//            System.out.println("Past 0 Y");
+//            double rando = Math.random()* 20 + 180;
+//            line.turnLeft(rando);
+//            line.setColor(new Color(r, g, b));
+//                    }
+//                }
+    }
 
     //Done for the most part
     public void Project07(){
---
+++ Projects/src/me/Jack/Projects/Project06.java
@@ -0,0 +1,43 @@
+package me.Jack.Projects;
+
+import java.awt.*;
+import java.util.Random;
+//import hsa.PaintBug;
+
+public class Project06 {
+        public static void main(String args[]){
+//            PaintBug.drawGrid();
+//            PaintBug.setWindowSize(600, 600);
+//            PaintBug line = new PaintBug();
+//            line.setSpeed(95);
+//            Random rand = new Random();
+//
+//            while(true){
+//                line.move(1);
+//                float r = rand.nextFloat();
+//                float g = rand.nextFloat();
+//                float b = rand.nextFloat();
+//                if(line.getXPos() >= 500){
+//                    System.out.println("Past 500 X");
+//                    double rando = Math.random()* 20 + 180;
+//                    line.turnLeft(rando);
+//                    line.setColor(new Color(r, g, b));
+//                } else if (line.getXPos() <= 0){
+//                    System.out.println("Past 0 X");
+//                    double rando = Math.random()* 20 + 180;
+//                    line.turnLeft(rando);
+//                    line.setColor(new Color(r, g, b));
+//                } else if (line.getYPos() >= 500){
+//                    System.out.println("Past 500 Y");
+//                    double rando = Math.random()* 20 + 180;
+//                    line.turnLeft(rando);
+//                    line.setColor(new Color(r, g, b));
+//                } else if (line.getYPos() <= 0){
+//                    System.out.println("Past 0 Y");
+//                    double rando = Math.random()* 20 + 180;
+//                    line.turnLeft(rando);
+//                    line.setColor(new Color(r, g, b));
+//                }
+//            }
+        }
+    }
